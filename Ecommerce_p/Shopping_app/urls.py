340c0ca0093a0d01439d39714119ff3f5e80433c
macbook air <macbookair@MacBook-Air-van-macbook.local>
2022-12-11 09:16:51 +0100

    yes

---
+++ Ecommerce_p/Shopping_app/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path
+from . import views
+app_name = 'Shopping_app'
+
+
+urlpatterns = [
+
+    path('shop',views.All_Cat,name='All_Cat'),
+    path('<slug:c_slug>',views.All_Cat,name='products_by_ category'),
+    path('<slug:c_slug>/<slug:p_slug>', views.All_product, name='products'),
+
+]

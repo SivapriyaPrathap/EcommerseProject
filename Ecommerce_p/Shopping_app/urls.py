d81c981f98ff7dcc45728088e87fbc86d8fa4097
macbook air <macbookair@MacBook-Air-van-macbook.local>
2023-01-20 19:40:23 +0100

    updated code

--- Ecommerce_p/Shopping_app/urls.py
+++ Ecommerce_p/Shopping_app/urls.py
@@ -6,7 +6,7 @@ app_name = 'Shopping_app'
 
 urlpatterns = [
 
-    path('shop',views.All_Cat,name='All_Cat'),
+    path('',views.All_Cat,name='All_Cat'),
     path('<slug:c_slug>',views.All_Cat,name='products_by_ category'),
     path('<slug:c_slug>/<slug:p_slug>', views.All_product, name='products'),
 
